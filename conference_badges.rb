07565c6c92d0ad27098484a8d54aa39757293142
Tabitha Sin <tabitha.sin@gmail.com>
2017-10-13 02:25:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,3 +20,12 @@ def assign_rooms(attendees)
   end
   rooms
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do badge
+    puts badge 
+  end
+  assign_rooms(attendees).each do rooms
+    puts rooms
+  end
+end
