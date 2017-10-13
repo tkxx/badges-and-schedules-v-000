65e78f0c6e216e01e8fd12a4348668de622e759c
Tabitha Sin <tabitha.sin@gmail.com>
2017-10-13 02:13:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,8 +14,8 @@ end
 
 def assign_rooms(attendees)
   rooms = []
-  attendees.collect.each_with_index do |names, room_number|
-   rooms << "Hello, #{names}! You'll be assigned to room #{room_number}!"
+  attendees.collect.each_with_index(1) do |names, room|
+   rooms << "Hello, #{names}! You'll be assigned to room #{room}!"
   end
   rooms
 end
