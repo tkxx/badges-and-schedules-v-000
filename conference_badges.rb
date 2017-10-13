7337d5289706203a271678baf4298d6cb768166b
Tabitha Sin <tabitha.sin@gmail.com>
2017-10-13 01:51:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+luminaries = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(luminaries)
+  badges = []
+  luminaries.each do |names|
+    badges <<  "Hello, my name is #{name}."
+  end
+  badges
+end
